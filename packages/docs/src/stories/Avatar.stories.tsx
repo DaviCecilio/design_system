2b679b28c82b9470a8d23124ab7e7c5f3aa1dad1
Davi Cecilio <davim.cecilio@gmail.com>
2023-11-01 20:25:43 -0300

    feat: add argTypes controls to stories

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://avatars.githubusercontent.com/u/53492145',
     alt: 'Diego Fernandes',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
