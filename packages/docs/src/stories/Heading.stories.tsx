3236fb1ef867d3c4937286ab943901ca4dc51ef4
Davi Cecilio <davim.cecilio@gmail.com>
2023-11-02 16:34:47 -0300

    chore: change lib name

--- packages/docs/src/stories/Heading.stories.tsx
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Heading, HeadingProps } from '@ignite-ui/react'
+import { Heading, HeadingProps } from '@davicecilio-ui/react'
 
 export default {
   title: 'Typography/Heading',
